e7328bddfaca96fba33164e47eececaf37c4bb4c
Ernesto <ferreyraernesto29@gmail.com>
2020-11-27 11:01:25 -0300

    Inicio de Proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Proximamente Ecommerce</h1>
+      <p>Articulos musicales</p>
+    </div>
+  );
+}
+
+export default App;
