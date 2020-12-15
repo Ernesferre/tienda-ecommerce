2b86ffeb6892168100902e217b4fecccc10c462a
Ernesto <ferreyraernesto29@gmail.com>
2020-12-14 23:53:53 -0300

    nuevo cambio

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import './ProductCard.css';
 const ProductCard = ({titulo, precio}) => {
     return (
         <article className="productCard">
-            <img src="https://placehold.it/300x300" alt="Mi producto de prueba"/>
+            <img src="https://placehold.it/400x400" alt="Mi producto de prueba"/>
             <div>
                 <h3>{titulo}</h3>
                 <p>${precio}</p>
