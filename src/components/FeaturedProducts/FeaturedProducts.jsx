23d1095f652e14b9cf6630acbd3a6e900b02814a
Ernesto <ferreyraernesto29@gmail.com>
2020-12-17 10:14:40 -0300

    llamado a una promise para que los items regresen desde una API en tiempo diferido emulando el retraso de red

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,18 +1,85 @@
+import {useState,useEffect} from "react";
 import ProductCard from '../../components/ProductCard/ProductCard';
 import './FeaturedProducts.css';
 
 const FeaturedProducts = () => {
+    
+    const [items, setItems] = useState ([]);
+
+    
+    // Hacer de cuenta que esta constante es una API
+    const products = [
+        {
+            id: 1,
+            titulo: "Guitarra Criolla",
+            Precio: 5900,
+        },
+        {
+            id: 2,
+            titulo: "Bateria 7 cuerpos",
+            Precio: 16300,
+        },
+        {
+            id: 3,
+            titulo: "Microfono Shure 200",
+            Precio: 8500,
+        },
+        {
+            id: 4,
+            titulo: "Bajo 4 cuerdas",
+            Precio: 12000,
+        },
+
+    ]
+
+    const getProducts = new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve(products);
+        }, 5000)
+    })
+
+    useEffect(() => {
+        getProducts.then(rta => setItems(rta));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    }, []);
+
+   
+
+
+
+    
+
+
     return (
         <section className="featuredProducts">
             <div className="container">
-                <h2>Alguno de Nuestros Productos</h2>
+                
 
-                <ul>
-                    <li><ProductCard titulo="Producto 1" precio="10.000" /></li>
-                    <li><ProductCard titulo="Producto 2" precio="20.000" /></li>
-                    <li><ProductCard titulo="Producto 3" precio="25.000" /></li>
-                    <li><ProductCard titulo="Producto 4" precio="30.000" /></li>
-                </ul>
+                
+
+                {
+                    items.length ?
+                        <>
+                            <h2>Alguno de Nuestros Productos</h2>
+
+                            <ul>
+                                {
+                                    items.map(item => (
+                                        <li key={item.id}>
+                                            <ProductCard
+                                                titulo={item.titulo} 
+                                                precio={item.Precio}
+                                            />
+                                        </li>
+                                    ))
+                                }
+                            </ul>    
+                        </>:
+                        
+                        
+                    <p className="cargando">Cargando Items...</p>
+                }
             </div>
         </section>
     )
